45283618780e18c685c8d8d3e54139af0dfa6175
ksone02 <eksovus@gmail.com>
2021-07-18 23:32:36 +0900

    Add Register Function

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,18 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Register.css';
 
 function Register() {
+
+    const [id, setId] = useState("");
+    const [pw, setPw] = useState("");
+    const [email, setEmail] = useState("");
+    const [user_name, setName] = useState("");
+    const [nickname, setNickname] = useState("");
+
+    const idCheck = (e) => {setId(e.target.value) };
+    const pwCheck = (e) => {setPw(e.target.value) };
+    const emailCheck = (e) => {setEmail(e.target.value) };
+    const nameCheck = (e) => {setName(e.target.value) };
+    const nicknameCheck = (e) => {setNickname(e.target.value) };
+
+    const submitId = () => {
+        const post = {
+            id: id,
+            pw: pw,
+            email: email,
+            user_name: user_name,
+            nickname: nickname
+        };
+        if(id === "" || pw === "" || email === "" || user_name === "" || nickname === "") {
+            alert("입력이 안 된 사항이 있습니다.");
+        } else {
+            fetch("http://localhost:3001/register", {
+                method: "post",
+                headers: {
+                    "content-type" : "application/json",
+                },
+                body: JSON.stringify(post),
+            })
+            .then((res) => res.json())
+            .then((json) => {
+                setId(json.text);
+            });    
+        }
+        
+    };
+
     return (
-        <form id="register-container" method="post" action="/register/agreement">
+        <form id="register-container" method="post" action={submitId}>
             <h2>에브리타임 회원가입</h2>
             <p className="description">에브리타임 계정으로 <strong>캠퍼스픽, 에브리타임</strong> 등<br />다양한 대학생 서비스를 모두 이용하실 수 있습니다.</p>
             <div class="input">
                 <div class="label"><label>아이디</label></div>
-                <input type="text" name="user_id" maxlength="20" placeholder="아이디를 입력하세요." className="search" />
+                <input onChange={idCheck} type="text" name="user_id" maxlength="20" placeholder="아이디를 입력하세요." className="search" />
             </div>
             <div class="input">
                 <div class="label"><label>비밀번호</label></div>
-                <input type="password" name="user_pw" maxlength="20" placeholder="비밀번호를 입력하세요." className="search" />
+                <input onChange={pwCheck} type="password" name="user_pw" maxlength="20" placeholder="비밀번호를 입력하세요." className="search" />
             </div>
             <div class="input">
                 <div class="label"><label>비밀번호 확인</label></div>
@@ -20,19 +59,20 @@ function Register() {
             </div>
             <div class="input">
                 <div class="label"><label>이메일</label></div>
-                <input type="email" name="user_email" maxlength="50" placeholder="이메일을 입력하세요." className="search" />
+                <input onChange={emailCheck} type="email" name="user_email" maxlength="50" placeholder="이메일을 입력하세요." className="search" />
             </div>
             <div class="input">
                 <div class="label"><label>이름</label></div>
-                <input type="text" name="user_name" maxlength="20" placeholder="이름을 입력하세요." className="search" />
+                <input onChange={nameCheck} type="text" name="user_name" maxlength="20" placeholder="이름을 입력하세요." className="search" />
             </div>
             <div class="input">
                 <div class="label"><label>닉네임</label></div>
-                <input type="text" name="user_nickname" maxlength="20" placeholder="닉네임(별명)을 입력하세요." className="search" />
+                <input onChange={nicknameCheck} type="text" name="user_nickname" maxlength="20" placeholder="닉네임(별명)을 입력하세요." className="search" />
             </div>
             <input type="submit" value="가입하기"></input>
+            <h1>{id}</h1>
         </form>
-    )
+    )    
 }
 
 export default Register
--- server/server.js
+++ server/server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3001; // react의 기본값은 3000이니까 3000이 아닌 아무 수
 const mysql = require("mysql"); // mysql 모듈 사용
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(bodyParser.json());
 
 var connection = mysql.createConnection({
     host : "localhost",
@@ -14,25 +20,36 @@ connection.connect();
 
 app.get('/', (req, res) =>{
     res.send('서버 온!')
-})
+});
 
-app.post("/idplz", (req,res)=>{
-    const id = req.body.id;
-    // console.log(req.body);
-    connection.query("INSERT INTO users (user_id) values (?)",[id],
-    function(err,rows,fields){
-        if(err){
-            console.log("실패");
-            // console.log(err);
-        }else{
-            console.log("성공");
-            // console.log(rows);
-        };
-    });
+app.post("/register", (req,res)=>{
+    var today = new Date();
+    var users = {
+        "user_id": req.body.id,
+        "user_password": req.body.pw,
+        "user_email": req.body.email,
+        "user_name": req.body.user_name,
+        "user_nickname": req.body.nickname,
+        "created_time": today,
+    }
 
-    
+    connection.query('INSERT INTO users SET ?' , users, function (error, results, fields) {
+        if (error) {
+            console.log("error ocurred", error);
+            res.send({
+                "code" : 400,
+                "failed": "error ocurred"
+            })
+        } else {
+            console.log('The solution is: ', results);
+            res.send({
+                "code": 200,
+                "success": "user registered sucessfully"
+            });
+        }
+    });    
 });
 
 app.listen(port, ()=>{
     console.log(`Connect at http://localhost:${port}`);
-})
+});
